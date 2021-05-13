2852422bb61cdc67ae30a15bb9f57e325729da78
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-05-12 20:44:48 -0700

    update links, and testimonial section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 let menuBtn = document.getElementsByClassName("menu-button");
 let mobileMenu = document.getElementsByClassName("mobile-menu");
 
+
+
 menuBtn[0].addEventListener("click", () => {
   mobileMenu[0].classList.toggle("active");
 });
@@ -14,3 +16,45 @@ window.addEventListener('click', function(e){
 }
 })
 console.log(menuBtn[0]);
+
+
+
+// TESTIMONIAL SECTION -- Switching person info on click
+window.addEventListener('click', function(e) {
+  //// Left Side
+  if (document.getElementById('test-left').contains(e.target)) {
+    let tempName = document.getElementById('left-name').innerHTML;
+    let tempDetail = document.getElementById('left-user-detail').innerHTML;
+    let tempAvatar = document.getElementById('left-image').style.backgroundImage;
+    let tempTesti = document.getElementById('left-testimonial').innerHTML;
+    document.getElementById('left-name').innerHTML = document.getElementById('user-name').innerHTML;
+    document.getElementById('left-image').style.backgroundImage = document.getElementById('user-avatar').style.backgroundImage;
+    document.getElementById('left-user-detail').innerHTML = document.getElementById('user-detail').innerHTML;
+    document.getElementById('left-testimonial').innerHTML = document.getElementById('user-testimonial').innerHTML;
+
+    document.getElementById('user-name').innerHTML = tempName;
+    document.getElementById('user-detail').innerHTML = tempDetail;
+    document.getElementById('user-avatar').style.backgroundImage = tempAvatar;
+    document.getElementById('user-testimonial').innerHTML = tempTesti;
+  }
+  //// Right Side
+  if (document.getElementById('test-right').contains(e.target)) {
+    let tempName = document.getElementById('right-name').innerHTML;
+    let tempDetail = document.getElementById('right-user-detail').innerHTML;
+    let tempAvatar = document.getElementById('right-image').style.backgroundImage;
+    let tempTesti = document.getElementById('right-testimonial').innerHTML;
+    document.getElementById('right-name').innerHTML = document.getElementById('user-name').innerHTML;
+    document.getElementById('right-image').style.backgroundImage = document.getElementById('user-avatar').style.backgroundImage;
+    document.getElementById('right-user-detail').innerHTML = document.getElementById('user-detail').innerHTML;
+    document.getElementById('right-testimonial').innerHTML = document.getElementById('user-testimonial').innerHTML;
+
+    document.getElementById('user-name').innerHTML = tempName;
+    document.getElementById('user-detail').innerHTML = tempDetail;
+    document.getElementById('user-avatar').style.backgroundImage = tempAvatar;
+    document.getElementById('user-testimonial').innerHTML = tempTesti;
+  }
+
+}
+
+
+)
